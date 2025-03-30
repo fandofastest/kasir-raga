b68f50a3eba413f9557c40410896632fe8c56f4b
Rifando Panggabean <fandofast@gmail.com>
2025-03-30 10:25:58 +0700

    f

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
-import plugin from "tailwindcss/plugin";
 
 const colors = require("tailwindcss/colors");
 
@@ -346,6 +345,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+  // plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
 export default config;
